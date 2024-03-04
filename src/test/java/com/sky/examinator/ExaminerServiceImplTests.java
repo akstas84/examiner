49d697305b494989fd239b3acfee65cc68e44f35
Stanislav <akstas84@users.noreply.github.com>
2024-03-04 23:18:09 +0300

    add branch with test getQuest

--- src/test/java/com/sky/examinator/ExaminerServiceImplTests.java
+++ src/test/java/com/sky/examinator/ExaminerServiceImplTests.java
@@ -2,10 +2,16 @@ package com.sky.examinator;
 
 import com.sky.examinator.service.ExaminerServiceImpl;
 import com.sky.examinator.service.QuestionService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import static com.sky.examinator.TestData.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -17,5 +23,11 @@ class ExaminerServiceImplTests {
     @InjectMocks
     ExaminerServiceImpl out;
 
+
+    @Test
+    void getQuestions() {
+        when(mock.getRandomQuestion()).thenReturn(FULL_QUESTION3);
+        assertTrue(out.getQuestions(ID).contains(FULL_QUESTION3));
+    }
 
 }
